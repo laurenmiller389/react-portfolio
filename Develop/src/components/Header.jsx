c82b5e2c957aa2103e44ed2bc11bac47fb2a5257
Lauren Miller <lmiller90@fordham.edu>
2023-07-24 19:57:32 -0400

    provided project data to projects array

--- Develop/src/components/Header.jsx
+++ Develop/src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
-      <header className="mb-4 py-3 flex-row align-center">
-        <div className="container flex-row justify-space-between-lg justify-center align-center" id="header">
+        <header>
+        <div className="container flex-row justify-space-between-lg justify-center align-center" id="header"> 
           <h1 className="m-0">Lauren Miller</h1>
           <p className="m-0">Hi my name is Lauren Miller here is a little about me....</p>
         </div>
--- Develop/src/components/Portfolio.jsx
+++ Develop/src/components/Portfolio.jsx
@@ -4,6 +4,7 @@ function ProjectComponent({title, link, image}) {
         <div>
             <h2>{title}</h2>
             <img src={image}></img>
+            <a href={link}></a>
         </div>
     );
 }
@@ -11,20 +12,30 @@ function ProjectComponent({title, link, image}) {
 export default function Portfolio() {
     const projects = [
         {
-            title: "Random",
-            link: "random link",
-            image: "image src"
+            title: "The Breakroom",
+            image: "./theBreakroom.png",
+            link: "https://sefenydiaz.github.io/test-arcade/index.html"
         },
         {
-            title: "Random",
-            link: "random link",
-            image: "image src"
+            title: "Movie Mentor",
+            image: "./movieMenor",
+            link: "https://github.com/tegaomare/moviementor"
+        },
+        {
+            title: "Work Day Scheduler",
+            image: "./workDayScheduler",
+            link: "https://github.com/laurenmiller389/hw5-workday"
         },
     ];
 
     return (
-        projects.map((project,index) => (
-            <ProjectComponent key={index} title={project.title} link={project.link}/>
-    ))
+        {projects.map((project,index) => (
+            <ProjectComponent 
+            key={index} 
+            title={project.title} 
+            link={project.link}
+            image={project.image}
+            />
+    ))}
     )
 }
