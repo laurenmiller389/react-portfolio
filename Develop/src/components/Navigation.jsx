b508066288e3733be209c7d7c657076c0dbbddf6
Lauren Miller <lmiller90@fordham.edu>
2023-07-24 21:11:31 -0400

    wrote out the about page

--- Develop/src/components/Navigation.jsx
+++ Develop/src/components/Navigation.jsx
@@ -2,7 +2,13 @@
 
 function NavTabs({ currentPage, handlePageChange }) {
     return (
-        <navbar id="navbar">
+        
+<div className="container">
+  <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+    <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+      <svg className="bi me-2" width="40" height="32"><use xlinkHref="#bootstrap"/></svg>
+      <span className="fs-4">Lauren Miller</span>
+    </a>
       <ul className="nav nav-tabs">
         <li className="nav-item">
           <a
@@ -46,7 +52,8 @@ function NavTabs({ currentPage, handlePageChange }) {
           </a>
         </li>
       </ul>
-      </navbar>
+      </header>
+  </div>
     );
   }
   
