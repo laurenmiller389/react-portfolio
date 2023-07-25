e48e612c0adb814f576e8f423f59d785c7752f81
Lauren Miller <lmiller90@fordham.edu>
2023-07-25 15:50:00 -0400

    added resume code to its jsx file

--- Develop/src/components/Resume.jsx
+++ Develop/src/components/Resume.jsx
@@ -1,1 +1,16 @@
 //link to downloadable version of resume
+
+
+const Resume = () => {
+    return (
+      <div className="container mt-5">
+        <h2>My Resume</h2>
+        <p>Here is the link:</p>
+        <a href="https://docs.google.com/document/d/1VQKbCWw1eu3C-qPl_GJTWEFRvcU_XGonYoRr7eMVJKs/edit?usp=sharing" target="_blank" className="btn btn-primary">
+          View Resume
+        </a>
+      </div>
+    );
+  };
+  
+  export default Resume;
