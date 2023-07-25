a5e974e59cc239a2b82f473abd685946bc1c993d
Lauren Miller <lmiller90@fordham.edu>
2023-07-24 22:02:47 -0400

    added my own info and customized contact form

--- Develop/src/components/Contact.jsx
+++ Develop/src/components/Contact.jsx
@@ -1,89 +1,59 @@
 const Contact = () => {
     return (
-<div className="col-md-7 col-lg-8">
-<h4 className="mb-3">Billing address</h4>
-<form className="needs-validation" noValidate>
-  <div className="row g-3">
-    <div className="col-sm-6">
-      <label htmlFor="firstName" className="form-label">First name</label>
-      <input type="text" className="form-control" id="firstName" placeholder="" value="" required></input>
-      <div className="invalid-feedback">
-        Valid first name is required.
-      </div>
-    </div>
-
-    <div className="col-sm-6">
-      <label htmlFor="lastName" className="form-label">Last name</label>
-      <input type="text" className="form-control" id="lastName" placeholder="" value="" required></input>
-      <div className="invalid-feedback">
-        Valid last name is required.
-      </div>
-    </div>
-
-    <div className="col-12">
-      <label htmlFor="username" className="form-label">Username</label>
-      <div className="input-group has-validation">
-        <span className="input-group-text">@</span>
-        <input type="text" className="form-control" id="username" placeholder="Username" required></input>
-      <div className="invalid-feedback">
-          Your username is required.
+        <div>
+        <div className="container px-4 py-5" id="icon-grid">
+        <h2 className="pb-2 border-bottom">Contact Me!</h2>
+    
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 py-5">
+          <div className="col d-flex align-items-start">
+            <svg className="bi text-body-secondary flex-shrink-0 me-3" width="1.75em" height="1.75em"><use xlink:href="#bootstrap"/></svg>
+            <div>
+              <h3 className="fw-bold mb-0 fs-4 text-body-emphasis">Email</h3>
+              <p>laurenmiller389@gmail.com</p>
+            </div>
+          </div>
+          <div className="col d-flex align-items-start">
+            <svg className="bi text-body-secondary flex-shrink-0 me-3" width="1.75em" height="1.75em"><use xlink:href="#cpu-fill"/></svg>
+            <div>
+              <h3 className="fw-bold mb-0 fs-4 text-body-emphasis">Phone</h3>
+              <p>203-535-9789</p>
+            </div>
+          </div>
+          <div className="col d-flex align-items-start">
+            <svg className="bi text-body-secondary flex-shrink-0 me-3" width="1.75em" height="1.75em"><use xlink:href="#calendar3"/></svg>
+            <div>
+              <h3 className="fw-bold mb-0 fs-4 text-body-emphasis">Address</h3>
+              <p>I ain't giving that to you !!!!</p>
+            </div>
+          </div>
+          
+          </div>
         </div>
-      </div>
-    </div>
+      
+      
 
-    <div className="col-12">
-      <label htmlFor="email" className="form-label">Email <span className="text-body-secondary">(Optional)</span></label>
-      <input type="email" className="form-control" id="email" placeholder="you@example.com"></input>
-      <div className="invalid-feedback">
-        Please enter a valid email address for shipping updates.
-      </div>
-    </div>
+            <div className="container mt-5">
+              <form>
+                <div className="form-group">
+                  <label htmlFor="name">Name:</label>
+                  <input type="text" className="form-control" id="name" placeholder="Enter your name" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="email">Email:</label>
+                  <input type="email" className="form-control" id="email" placeholder="Enter your email" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="message">Message:</label>
+                  <textarea className="form-control" id="message" rows="4" placeholder="Enter your message"></textarea>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+              </form>
+            </div>
+           
+        </div>
 
-    <div className="col-12">
-      <label htmlFor="address" className="form-label">Address</label>
-      <input type="text" className="form-control" id="address" placeholder="1234 Main St" required></input>
-      <div className="invalid-feedback">
-        Please enter your shipping address.
-      </div>
-    </div>
+           
+          );
+        };
 
-    <div className="col-12">
-      <label htmlFor="address2" className="form-label">Address 2 <span className="text-body-secondary">(Optional)</span></label>
-      <input type="text" className="form-control" id="address2" placeholder="Apartment or suite"></input>
-    </div>
-
-    <div className="col-md-5">
-      <label htmlFor="country" className="form-label">Country</label>
-      <select className="form-select" id="country" required>
-        <option value="">Choose...</option>
-        <option>United States</option>
-      </select>
-      <div className="invalid-feedback">
-        Please select a valid country.
-      </div>
-    </div>
-
-    <div className="col-md-4">
-      <label htmlFor="state" className="form-label">State</label>
-      <select className="form-select" id="state" required>
-        <option value="">Choose...</option>
-        <option>California</option>
-      </select>
-      <div className="invalid-feedback">
-        Please provide a valid state.
-      </div>
-    </div>
-
-    <div className="col-md-3">
-      <label htmlFor="zip" className="form-label">Zip</label>
-      <input type="text" className="form-control" id="zip" placeholder="" required></input>
-      <div className="invalid-feedback">
-        Zip code required.
-      </div>
-    </div>
-  </div>
-  </form>
-  </div>
-  )
-  }
   export default Contact;
